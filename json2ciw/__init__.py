634bd0debf8b36353b97f990dbb8d10d1d179940
amyheather <a.heather2@exeter.ac.uk>
2026-06-19 12:09:52 +0100

    fix(__init__): remove __all__, use automated detection - it was outdated (missing ProcessModel, various loading functions) but still worked and found those regardless, so felt a bit pointless/misleading? Only thing it seemed to be doing was hiding things from API reference that actually were accessible

--- json2ciw/__init__.py
+++ json2ciw/__init__.py
@@ -1,20 +1,3 @@
 """json2ciw."""
 
 __version__ = "0.10.0"
-
-from .datasets import load_call_centre_model, load_model_file
-from .engine import CiwConverter, multiple_replications
-from .results import summarise_results, tidy_to_wide_format, create_user_filtered_hist
-from .ui import render_simulation_app
-
-# Only these objects will be possible to import from package:
-__all__ = [
-    "load_model_file",
-    "load_call_centre_model",
-    "CiwConverter",
-    "multiple_replications",
-    "summarise_results",
-    "render_simulation_app",
-    "tidy_to_wide_format",
-    "create_user_filtered_hist",
-]
